e262a1bed4a570e6b0807c23b88ca933cb487050
elena <elenapolishchukdev@gmail.com>
2025-04-22 20:47:33 +0300

    common.js export style

---
+++ service/validate.js
@@ -0,0 +1,22 @@
+const validateEcmascriptText = (text) => {
+    const pattern = /^ *ECMAScript([1-9]|1[0-5])?$/i;
+    return pattern.test(text);
+}
+
+const validateDomainZone = (text) => {
+    const pattern = /^(\w|[.-])+\.(com|org|il)$/i; // a-z0-9_   =   \w
+    return pattern.test(text);
+}
+
+const validateEmail = (email) => {
+    const pattern = /^\w(\w|\.|-)*@[a-z0-9](\w|-|\.)*\.[a-z]{2,6}$/i
+    return pattern.test(email);
+}
+
+module.exports = {
+    validateEcmascriptText,
+    validateDomainZone,
+    validateEmail
+}; // common.js export style
+
+// ecmoscript export style
---
+++ service/validate.test.js
@@ -0,0 +1,61 @@
+const {validateEcmascriptText, validateDomainZone, validateEmail} = require('./validate'); // common.js export style
+
+
+describe('validateEcmascriptText', () => {
+    test('should return true for valid ECMAScript is provided', () => {
+        expect(validateEcmascriptText('       ecmascript')).toBe(true);
+        expect(validateEcmascriptText('ecmascript')).toBe(true);
+        expect(validateEcmascriptText('       ecmascript15')).toBe(true);
+        expect(validateEcmascriptText('       ecmascript7')).toBe(true);
+        expect(validateEcmascriptText('ECMAScript')).toBe(true);
+    })
+
+    test('should return false for valid ECMAScript is not provided', () => {
+        expect(validateEcmascriptText('   ecmascrip')).toBe(false);
+        expect(validateEcmascriptText('ecmascript0')).toBe(false);
+        expect(validateEcmascriptText('       ecmascript16')).toBe(false);
+        expect(validateEcmascriptText('   10    ecmascript7')).toBe(false);
+    })
+})
+
+describe('validateDomainZone', () => {
+    test('domain zone is valid', () => {
+        expect(validateDomainZone('abracadabra.com')).toBe(true);
+        expect(validateDomainZone('abracadabra.org')).toBe(true);
+        expect(validateDomainZone('abracadabra.il')).toBe(true);
+        expect(validateDomainZone('abra-cadabra.il')).toBe(true);
+        expect(validateDomainZone('abra.cadabra.il')).toBe(true);
+        expect(validateDomainZone('abra_cadabra.il')).toBe(true);
+        expect(validateDomainZone('abracadabra2025.il')).toBe(true);
+    })
+
+    test(' domain zone is not valid', () => {
+        expect(validateDomainZone('abracadabracom')).toBe(false);
+        expect(validateDomainZone('abracadabra.ru')).toBe(false);
+        expect(validateDomainZone('.com')).toBe(false);
+        expect(validateDomainZone('abraca dabra.com')).toBe(false);
+        expect(validateDomainZone('abraca$dabra.com')).toBe(false);
+    })
+})
+
+describe('validateEmail', () => {
+    test ('valid email address', () => {
+        expect(validateEmail('john-smith1990@gmail.com')).toBe(true);
+        expect(validateEmail('o@g.uk')).toBe(true);
+        expect(validateEmail('john.smith@walla.co.il')).toBe(true);
+    })
+
+    test('not valid email address', () => {
+        expect(validateEmail('abracadabra.com')).toBe(false);
+        expect(validateEmail('.com')).toBe(false);
+        expect(validateEmail('abracadabr@@a.com')).toBe(false);
+        expect(validateEmail('abra@cada@bra.com')).toBe(false);
+        expect(validateEmail('@gmail.com')).toBe(false);
+        expect(validateEmail('abracadabra@gmail.c')).toBe(false);
+        expect(validateEmail('abracad$abraa@gmail.com')).toBe(false);
+        expect(validateEmail('abracad abraa@gmail.com')).toBe(false);
+        expect(validateEmail('abracadabraa@gmail.commerce')).toBe(false);
+        expect(validateEmail('abra@gmailcom')).toBe(false);
+        expect(validateEmail('abr.a@gmailcom')).toBe(false);
+    })
+})
